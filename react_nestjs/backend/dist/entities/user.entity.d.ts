d1f5f0207e66d76656beb4856cbd8382065d67f2
junseok-seo <js_seo@opstech.kr>
2025-03-30 17:47:58 +0900

    delete dist

--- react_nestjs/backend/dist/entities/user.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Post } from './post.entity';
-export declare class User {
-    id: number;
-    username: string;
-    password: string;
-    email: string;
-    createdAt: Date;
-    posts: Post[];
-}
--- react_nestjs/backend/dist/users/entities/user.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { Post } from '../../../entities/post.entity';
-export declare class User {
-    id: number;
-    username: string;
-    email: string;
-    password: string;
-    posts: Post[];
-}
